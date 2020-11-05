681e8431268c7a409217b7181ae0a3f70e300621
Jack Yen <jack.yen@getac.com.tw>
2020-11-05 10:06:53 +0800

    Create webpack config for server

---
+++ webpack.server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  entry: './server/index.js',
+  externals: [webpackNodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'server.js'
+  }
+};
