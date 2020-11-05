ca9a59a9cf58fb9bbb9532a0d70bfba92b08545c
Jack Yen <jack.yen@getac.com.tw>
2020-11-05 14:53:58 +0800

    Revise User component
    Fetch users when User component rendered, and show in page.

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { fetchUsers } from "../actions/UserAction";
 
-const UserList = () => {
+const User = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users);
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <>
       <h1>User Page</h1>
       <Link to="/">To home page</Link>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.first_name} </li>
+        ))}
+      </ul>
     </>
   );
 };
 
-export default UserList;
+export default User;
